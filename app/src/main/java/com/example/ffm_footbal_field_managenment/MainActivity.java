e7eb3f2465f6ec30f6f137d69740faf62b1ec951
ITKenTo <107255169+ITKenTo@users.noreply.github.com>
2022-11-14 18:37:34 +0700

    FFM project

---
+++ app/src/main/java/com/example/ffm_footbal_field_managenment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ffm_footbal_field_managenment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
